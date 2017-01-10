2804b1c1e8866ea20c9ea4c09e5f9245dccf03de
simutaroman <simutaroman@gmail.com>
2017-01-10 21:35:02 +0200

    header content added to less task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,46 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
+"use strict"
+
+let gulp = require("gulp");
+let less = require("gulp-less");
+let header = require("gulp-header");
 
 
-// Compile LESS files from ./less into ./css
+let pkg = require("./package.json");
+
+// Sets the header content
+var devheader = ["/*!\n",
+    " * Bootstrap Seed - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.repository%>)\n",
+    " * Copyright " + (new Date()).getFullYear(), " <%= pkg.author %>\n",
+    " * Licensed under <%= pkg.license %>\n",
+    " */\n",
+    ""
+].join("");
+
+
+// Compiles LESS files from ./less into ./css
 gulp.task("less", function() {
     return gulp.src("less/custom.less")
         .pipe(less())
+        .pipe(header(devheader, { pkg: pkg }))
         .pipe(gulp.dest("css"))
 });
 
 
 gulp.task("copy-vendor", function(){
-    // copy bootstrap
+    // copies bootstrap
     gulp.src([
         "node_modules/bootstrap/dist/**/*",
         "!**/npm.js", "!**/bootstrap-theme.*",
         "!**/*.map"
     ])
     .pipe(gulp.dest("vendor/bootstrap"))
-    // copy jquery
+    // copies jquery
     gulp.src([
         "node_modules/jquery/dist/jquery.js",
         "node_modules/jquery/dist/jquery.min.js"
     ])
     .pipe(gulp.dest("vendor/jquery"))
-    // copy font-awesome
+    // copies font-awesome
     gulp.src([
         "node_modules/font-awesome/**",
         "!node_modules/font-awesome/**/*.map",
@@ -36,5 +52,5 @@ gulp.task("copy-vendor", function(){
     .pipe(gulp.dest("vendor/font-awesome"))
 });
 
-// Run everything
+// runs everything
 gulp.task("default", ["less", "copy-vendor"]);
