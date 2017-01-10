ff86c35611eb5862ef07101d9dcec9b60dc4e88d
simutaroman <simutaroman@gmail.com>
2017-01-10 19:00:49 +0200

    gulp task added for copying bootstrap, jquery and font-awesome vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,31 @@ gulp.task("less", function() {
 });
 
 
+gulp.task("copy-vendor", function(){
+    // copy bootstrap
+    gulp.src([
+        "node_modules/bootstrap/dist/**/*",
+        "!**/npm.js", "!**/bootstrap-theme.*",
+        "!**/*.map"
+    ])
+    .pipe(gulp.dest("vendor/bootstrap"))
+    // copy jquery
+    gulp.src([
+        "node_modules/jquery/dist/jquery.js",
+        "node_modules/jquery/dist/jquery.min.js"
+    ])
+    .pipe(gulp.dest("vendor/jquery"))
+    // copy font-awesome
+    gulp.src([
+        "node_modules/font-awesome/**",
+        "!node_modules/font-awesome/**/*.map",
+        "!node_modules/font-awesome/.npmignore",
+        "!node_modules/font-awesome/*.txt",
+        "!node_modules/font-awesome/*.md",
+        "!node_modules/font-awesome/*.json"
+    ])
+    .pipe(gulp.dest("vendor/font-awesome"))
+});
+
 // Run everything
-gulp.task("default", ["less"]);
+gulp.task("default", ["less", "copy-vendor"]);
