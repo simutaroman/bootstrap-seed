792f7208e82c481ffa0759b6227d81c75a015811
simutaroman <simutaroman@gmail.com>
2017-01-11 16:02:30 +0200

    js and css minifications, browserSync, todo

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@
 let gulp = require("gulp");
 let less = require("gulp-less");
 let header = require("gulp-header");
-
+let cleanCSS = require("gulp-clean-css");
+let rename = require("gulp-rename");
+let uglify = require("gulp-uglify");
+let browserSync = require("browser-sync").create();
 
 let pkg = require("./package.json");
+
+let customname = "custom";
 
 // Sets the header content
 var devheader = ["/*!\n",
@@ -18,28 +23,58 @@ var devheader = ["/*!\n",
 
 
 // Compiles LESS files from ./less into ./css
-gulp.task("less", function() {
-    return gulp.src("less/custom.less")
+gulp.task("less", function () {
+    return gulp.src(`less/${customname}.less`)
         .pipe(less())
         .pipe(header(devheader, { pkg: pkg }))
         .pipe(gulp.dest("css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// TODO: minify group of css files
+// minifies custom css file
+gulp.task("minify-custom-css", ["less"], function () {
+    return gulp.src(`css/${customname}.css`)
+        .pipe(cleanCSS({ compability: "ie8" }))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
+});
+
+// TODO: minify group of js files
+// minifies custom js file
+gulp.task("minify-custom-js", function () {
+    return gulp.src(`js/${customname}.js`)
+        .pipe(uglify())
+        .pipe(header(devheader, { pkg: pkg }))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("js"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
 });
 
 // copies main dependencies to ./vendor folder
-gulp.task("copy-vendor", function(){
+gulp.task("copy-vendor", function () {
     // copies bootstrap
     gulp.src([
         "node_modules/bootstrap/dist/**/*",
         "!**/npm.js", "!**/bootstrap-theme.*",
         "!**/*.map"
     ])
-    .pipe(gulp.dest("vendor/bootstrap"))
+        .pipe(gulp.dest("vendor/bootstrap"))
     // copies jquery
     gulp.src([
         "node_modules/jquery/dist/jquery.js",
         "node_modules/jquery/dist/jquery.min.js"
     ])
-    .pipe(gulp.dest("vendor/jquery"))
+        .pipe(gulp.dest("vendor/jquery"))
     // copies font-awesome
     gulp.src([
         "node_modules/font-awesome/**",
@@ -49,8 +84,16 @@ gulp.task("copy-vendor", function(){
         "!node_modules/font-awesome/*.md",
         "!node_modules/font-awesome/*.json"
     ])
-    .pipe(gulp.dest("vendor/font-awesome"))
+        .pipe(gulp.dest("vendor/font-awesome"))
 });
 
+// Configuring the browserSync task
+gulp.task("browserSync", function () {
+    browserSync.init({
+        server: {
+            baseDir: ""
+        },
+    })
+})
 // runs everything
-gulp.task("default", ["less", "copy-vendor"]);
+gulp.task("default", ["browserSync", "less", "minify-custom-css", "minify-custom-js", "copy-vendor"]);
---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// Custom scripts
+
+(function($){
+    $("body").load(
+        console.log("Hello, bootstapped world!")
+    )
+}(jQuery))
